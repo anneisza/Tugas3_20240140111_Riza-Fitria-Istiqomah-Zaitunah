5712f59e29527ad24c75801c925178fa5c7dfdcc
anneisza <rizaanneisza@gmail.com>
2026-04-01 09:32:18 +0700

    Praktikum 4

---
+++ script.js
@@ -0,0 +1,46 @@
+function kirimData() {
+    var nama = document.getElementById("nama").value;
+    var nim = document.getElementById("nim").value;
+    var angkatan = document.getElementById("angkatan").value;
+
+    var peminatan = document.querySelector("input[name=peminatan]:checked");
+    var tanggal = document.getElementById("tanggal").value;
+    var alamat = document.getElementById("alamat").value;
+
+
+//validasi semua kolom terisi
+if (!nama || !nim || !angkatan || !tanggal || !alamat) {
+    alert("Mohon melengkapi semua data!");
+    return;
+}
+// Memastikan peminatan sudah dipilih
+if (!peminatan) {
+    alert("Mohon pilih peminatan!")
+    return;
+}
+var nilaiPeminatan = peminatan.value;
+
+alert(
+    "Nama : " + nama +
+    "\nNIM : " + nim +
+    "\nAngkatan : " + angkatan +
+    "\nPeminatan : " + nilaiPeminatan +
+    "\nTanggal : " + tanggal +
+    "\nAlamat : " + alamat
+);
+
+clearForm();
+}
+
+function clearForm() {
+    document.getElementById("nama").value="";
+    document.getElementById("nim").value="";
+    document.getElementById("angkatan").value="";
+    document.getElementById("tanggal").value="";
+    document.getElementById("alamat").value="";
+
+    var radioButtons = document.querySelectorAll('input[name="peminatan"]');
+    for (var i = 0; i < radioButtons.length; i++) {
+        radioButtons[i].checked = false;
+    }
+}
